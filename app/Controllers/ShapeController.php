33341010a1ca600a0e215f86d314389bbfc5775c
Mufti Arfan Farooqi <muftiarfan@users.noreply.github.com>
2020-11-29 10:04:37 +0300

    Add hexagon shape
    Added hexagon shape code which was missing.

--- app/Controllers/ShapeController.php
+++ app/Controllers/ShapeController.php
@@ -21,12 +21,26 @@ class ShapeController extends Controller
             20, // x4, bottom-left
             230 // y4
         );
+
+        $points_hex = array(
+            175,38,
+            75,38,
+            25,125,
+            75,212,
+            175,212,
+            225,125,
+        );
       
         $color = imagecolorallocate($image, 255, 45, 255);
         $color_circle = imagecolorallocate($image, 0, 255, 255);
+        $color_hex = imagecolorallocate($image, 0, 71, 75);
+
         imagefilledpolygon($image, $points_sq, 4, $color ); //outside square
+
+        imagepolygon($image, $points_hex, 6, $color_hex ); //HEX
         imagefilledellipse ($image, 120, 120, 135, 135, $color_circle); //circle        
         imagerectangle($image, 80, 80, 160, 160, $color_sq2 ); //sq within circle
+        
         $this->response->setHeader('Content-type', 'image/png');
         imagepng($image);
         imagedestroy($image);
